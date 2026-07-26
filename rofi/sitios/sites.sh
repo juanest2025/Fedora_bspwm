ed0165642b7f3c830a5c8db298beaaa24067bbd7
juanest2025 <velezhincapiejuan@gmail.com>
2026-07-26 12:54:56 -0500

    up

--- rofi/sitios/sites.sh
+++ rofi/sitios/sites.sh
@@ -52,6 +52,7 @@ declare -A sites=(
   ["Love Next Door 2"]="https://doramasflix.in/doramas-online/love-next-door"
   ["King the land"]="https://www.doramasyt.com/dorama/king-the-land-latino-sub-espanol"
   ["La reina de las lagrimas"]="https://www.doramasyt.com/dorama/la-reina-de-las-lagrimas-latino-sub-espanol"
+  ["Lovely runner"]="https://www.doramasyt.com/dorama/corredora-encantadora-latino-sub-espanol"
   ["XVideos"]="https://www.xvideos.com/"
   ["Pornhub"]="https://es.pornhub.com/"
   ["Google Docs"]="https://docs.new"
