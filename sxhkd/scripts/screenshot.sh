73f6b0d04a7d2b2830711f787e3598904b84e5d1
juanest2025 <velezhincapiejuan@gmail.com>
2026-07-29 17:13:01 -0500

    up

--- sxhkd/scripts/screenshot.sh
+++ sxhkd/scripts/screenshot.sh
@@ -1,17 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 DIR="$HOME/Pictures/Screenshots"
-FILE="$DIR/$(date '+%Y-%m-%d_%H-%M-%S').png"
+FILE="$DIR/$(date +'%Y-%m-%d_%H-%M-%S').png"
 
 mkdir -p "$DIR"
 
 case "$1" in
-  full)
-    grim "$FILE"
-    wl-copy < "$FILE"
-    ;;
-  area)
-    grim -g "$(slurp)" "$FILE"
-    wl-copy < "$FILE"
-    ;;
+    full)
+        maim -u "$FILE"
+        ;;
+    area)
+        maim -s -u "$FILE"
+        ;;
+    window)
+        maim -i "$(xdotool getactivewindow)" -u "$FILE"
+        ;;
+    *)
+        echo "Uso: $0 {full|area|window}"
+        exit 1
+        ;;
 esac
+
+# Copiar al portapapeles
+xclip -selection clipboard -t image/png < "$FILE"
+
+# Notificación
+notify-send \
+    -i "$FILE" \
+    "📸 Captura realizada" \
+    "$(basename "$FILE")"
+
+exit 0
